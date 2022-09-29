27ee62df95bdc20088f858fcde2d0b642c24639e
viviannemelo <viviannemmelo@gmail.com>
2022-09-29 16:41:26 -0300

    Resolução requisito 01: getSpeciesByIds.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  return data.species.filter((specie) => ids.some((id) => specie.id === id));
 }
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+// 'e8481c1d-42ea-4610-8e11-1752cfc05a46',));
 
 module.exports = getSpeciesByIds;
