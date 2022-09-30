c657151bd2663bcac05e7f870ce4b97a01f4e00b
viviannemelo <viviannemmelo@gmail.com>
2022-09-30 19:02:36 -0300

    Requisito 08: calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+// const entrants = [
+//   { name:  'Lara Carvalho', age:  5 },
+//   { name:  'Frederico Moreira', age:  5 },
+//   { name:  'Pedro Henrique Carvalho', age:  5 },
+//   { name:  'Maria Costa', age:  18 },
+//   { name:  'Núbia Souza', age:  18 },
+//   { name:  'Carlos Nogueira', age:  50 },
+// ];
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const qntChild = entrants.filter((entrada) => entrada.age < 18).length;
+  const qntAdults = entrants.filter((entrada) => entrada.age >= 18 && entrada.age < 50).length;
+  const qntSenior = entrants.filter((entrada) => entrada.age >= 50).length;
+  const resultado = {
+    child: qntChild,
+    adult: qntAdults,
+    senior: qntSenior,
+  };
+  return resultado;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const priceChild = countEntrants(entrants).child * prices.child;
+  const priceAdult = countEntrants(entrants).adult * prices.adult;
+  const priceSenior = countEntrants(entrants).senior * prices.senior;
+  const total = priceChild + priceAdult + priceSenior;
+  console.log(total);
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,10 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verificar se é uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+  it('Verificar se parâmetro é diferente de string', () => {
+    expect(handlerElephants(0)).toBe('Valor inválido, é necessário uma string');
+  });
+});
