4e79ed39c1ada2b394acd5d06ff8c8cb6c1de0e2
viviannemelo <viviannemmelo@gmail.com>
2022-09-30 15:15:54 -0300

    Requisito 04: getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,6 +1,7 @@
 // const { verify } = require('crypto');
 // const { employees } = require('../data/zoo_data');
-// const data = require('../data/zoo_data');
+const data = require('../data/zoo_data');
+
 const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
 const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
 const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
@@ -11,10 +12,15 @@ function isManager(id) {
 }
 console.log(isManager(stephanieId));
 
-// function getRelatedEmployees(managerId) {
-//   let mensagem = 'O id inserido não é de uma pessoa colaboradora gerente!';
-//   managers.filter((pessoa) => (
-//     pessoa === managerId)).map((pessoa) => mensagem);
-// }
-// console.log(getRelatedEmployees(isManager));
-// module.exports = { isManager, getRelatedEmployees };
+function getRelatedEmployees(managerId) {
+  if (isManager(managerId) === true) {
+    return Object.values(data.employees)
+      .filter((managersName) => managersName.managers
+        .includes(managerId)).map((name) => `${name.firstName} ${name.lastName}`);
+  } if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+}
+
+console.log(getRelatedEmployees(burlId));
+module.exports = { isManager, getRelatedEmployees };
