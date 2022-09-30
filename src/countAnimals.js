21767b2217e56ca90b592743b872b4a7de8721c4
viviannemelo <viviannemmelo@gmail.com>
2022-09-30 17:17:11 -0300

    Requisito 05: countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+// const { count } = require('console');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const todosAnimais = {};
+    species.forEach((specie) => {
+      todosAnimais[specie.name] = specie.residents.length;
+    });
+    return todosAnimais;
+  }
+  if (typeof animal === 'object' && animal.sex !== undefined) {
+    return data.species.find((qntAnimais) => qntAnimais.name === animal.specie).residents
+      .filter((genero) => genero.sex === animal.sex).length;
+  } if (animal) {
+    return data.species.find((qntAnimais) => qntAnimais.name === animal.specie).residents.length;
+  }
 }
-
+console.log(countAnimals());
+// console.log(countAnimals({ specie: 'giraffes' }));
+// console.log(countAnimals({ specie: 'lions', sex: 'male' }));
 module.exports = countAnimals;
