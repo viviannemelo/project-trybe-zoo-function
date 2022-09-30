c4a7ef216aae3760ba87ad4bb43b9c395f638588
viviannemelo <viviannemmelo@gmail.com>
2022-09-30 20:21:00 -0300

    Requisito 6, 7 e 15 - handlerElephants.test.js

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,4 @@
+// const { it } = require('node:test');
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
@@ -5,6 +6,33 @@ describe('Testes da função HandlerElephants', () => {
     expect(typeof handlerElephants).toBe('function');
   });
   it('Verificar se parâmetro é diferente de string', () => {
-    expect(handlerElephants(0)).toBe('Valor inválido, é necessário uma string');
+    expect(handlerElephants(0)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('teste', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('teste', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('teste', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('teste', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('teste', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('teste', () => {
+    expect(handlerElephants('availability')).toStrictEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('teste', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+  it('teste', () => {
+    expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('teste', () => {
+    expect(handlerElephants('ahsjkddh')).toBe(null);
   });
 });
